78083e6c0503a6613ae3699b91f93f7127f44a49
Pratyush <pratyushjain1916@gmail.com>
2024-10-16 20:17:16 +0530

    Created a basic homepage layout

---
+++ client/src/App.js
@@ -0,0 +1,62 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="/" className="logo">MyLogo</a>
+        <nav>
+          <a href="/">Login</a>
+          <a href="/">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2024/10/Apple-iPad-mini-Apple-Intelligence-Image-Playground-241015.jpg?resize=2048,1524" alt="" />
+        </div>
+        <div className="texts">
+          <h2>New iPad mini brings support for Apple Intelligence and Pencil</h2>
+          <p className="info">
+            <a href="/" className="author">Pratyush Jain</a>
+            <time> 2023-01-06 16.45</time>
+          </p>
+          <p className="summary" >The new arrival marks the first major update to the 8.3-inch device since 2021. The A17 Pro, meanwhile, was announced last year, arriving on the iPhone 15 Pro. </p>
+        </div>
+      </div>
+      
+      
+      <div className="post">
+        <div className="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2024/10/Apple-iPad-mini-Apple-Intelligence-Image-Playground-241015.jpg?resize=2048,1524" alt="" />
+        </div>
+        <div className="texts">
+          <h2>New iPad mini brings support for Apple Intelligence and Pencil</h2>
+          <p className="info">
+            <a href="/" className="author">Pratyush Jain</a>
+            <time> 2023-01-06 16.45</time>
+          </p>
+          <p className="summary" >The new arrival marks the first major update to the 8.3-inch device since 2021. The A17 Pro, meanwhile, was announced last year, arriving on the iPhone 15 Pro. </p>
+        </div>
+      </div>
+      
+      
+      <div className="post">
+        <div className="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2024/10/Apple-iPad-mini-Apple-Intelligence-Image-Playground-241015.jpg?resize=2048,1524" alt="" />
+        </div>
+        <div className="texts">
+          <h2>New iPad mini brings support for Apple Intelligence and Pencil</h2>
+          <p className="info">
+            <a href="/" className="author">Pratyush Jain</a>
+            <time> 2023-01-06 16.45</time>
+          </p>
+          <p className="summary" >The new arrival marks the first major update to the 8.3-inch device since 2021. The A17 Pro, meanwhile, was announced last year, arriving on the iPhone 15 Pro. </p>
+        </div>
+      </div>
+      
+      
+    </main>
+  );
+}
+
+export default App;
