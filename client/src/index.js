0f027aef9b433f4e1182f9a4633522eee9283f75
Pratyush <pratyushjain1916@gmail.com>
2024-10-17 21:58:56 +0530

    Added routing using react router dom

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Layout from './Layout';
+import LoginPage from './pages/LoginPage';
+
+const router = createBrowserRouter([
+ {
+  element: <Layout />,
+  children: [
+    {
+      path: '/',
+      element: <Home />
+    },
+    {
+      path: '/login',
+      element: <LoginPage />
+    }
+  ]
+ }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ client/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Post from '../components/Post/Post'
+import '../App.css'
+
+
+const Home = () => {
+  return (
+    <>
+      <Post />
+      <Post />
+      <Post />
+    </>
+  )
+}
+
+export default Home
