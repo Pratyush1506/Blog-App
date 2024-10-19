5217d934034e5faf67f9e22ee80547a861c9ad8f
Pratyush <pratyushjain1916@gmail.com>
2024-10-19 21:31:01 +0530

    Added login functionality with JWT for session management

--- api/index.js
+++ api/index.js
@@ -5,10 +5,15 @@ const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 
 const app = express(); //
+const jwt = require('jsonwebtoken');
 
 const salt = bcrypt.genSaltSync(10);
+const secret = 'askj123kokn123';
 
-app.use(cors());
+app.use(cors({
+    credentials:true,
+    origin:'http://localhost:3000'
+}));
 
 app.use(express.json()); // parsing our body data
 
@@ -26,6 +31,24 @@ app.post('/register', async (req, res) => {
     } catch (error) {
         res.status(400).json(error);
     }
+});
+
+app.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password); // here we are comparing our password with the hased password we got from userDoc.
+    //res.json(passOk)
+    //res,json(userDoc)
+    if (passOk){
+        // logged in
+        jwt.sign({username, id:userDoc._id}, secret, {}, (err,token) => {
+            if(err) throw err;
+            res.cookie('token', token).json('ok');
+        });
+    } else {
+        res.status(400).json('wrong credentials');
+    }
+
 });
 
 // mongo DB password - UXvb9acvWj3riX0W Username - pratyush
